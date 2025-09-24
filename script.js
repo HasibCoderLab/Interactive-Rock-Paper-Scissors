fe15a35d78df93b3066da79eb65c7b94e81d7852
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-24 17:09:32 +0600

    Fixed id

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let imgs = document.querySelectorAll("img");
+let h2Tag = document.getElementById("head");
+let counter  = 1 ;
 imgs.forEach((image) => {
     image.addEventListener("click",() => {
         console.log("ah click marse");
-        
+        h2Tag.textContent = `Hello ${counter}`;
+       counter++
     })
 })
