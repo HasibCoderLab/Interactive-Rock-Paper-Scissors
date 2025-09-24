047ad82db1152f07e413d1aeccd9045093359a92
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-24 21:26:58 +0600

    add compurter choice

--- script.js
+++ script.js
@@ -11,16 +11,24 @@
 
 let userScore = 0;
 let compStore = 0;
-const playGame = (userChoice) => {
-    console.log(`user Choice is ${userChoice}`);
-
-}
-
 const choice = document.querySelectorAll(".choice");
 // console.log(choice);
+
+const genCompChoice = () => {
+    const Items = ["rock", "paper", "scissors"];
+    const randomIdx = Math.floor(Math.random() * 3)
+    return Items[randomIdx];
+}
+
+const playGame = (userChoice) => {
+    console.log(`user Choice is ${userChoice}`);
+    const compChoice = genCompChoice();
+    console.log(`comp Choice is ${compChoice}`);
+
+}
 choice.forEach((choice) => {
     choice.addEventListener("click", () => {
-        console.log("click");
+        // console.log("click");
         const userChoice = choice.getAttribute("id")
         // console.log("Your choice is ", userChoice);
 
